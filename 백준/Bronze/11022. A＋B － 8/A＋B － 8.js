30ab08a1cc3e7bc10bf0e32211a588f68b225dcc
민석 <70144179+minseok-seong@users.noreply.github.com>
2024-03-18 11:22:29 +0900

    [Bronze V] Title: A+B - 8, Time: 132 ms, Memory: 9540 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,16 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+let hap = 0;
+
+for (let i = 1; i < input.length; i++) {
+  let num = [];
+  for (let j = 0; j < input[i].length; j++) {
+    if (parseInt(input[i][j]) || parseInt(input[i][j] === 0)) {
+      hap += parseInt(input[i][j]);
+      num.push(input[i][j]);
+    }
+  }
+
+  console.log("Case #" + i + ": " + num[0] + " + " + num[1] + " = " + hap);
+  hap = 0;
+}
