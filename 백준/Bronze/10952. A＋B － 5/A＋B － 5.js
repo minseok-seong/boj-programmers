f2fb43ea569cecd4199474c7cf6166b91423c07e
민석 <70144179+minseok-seong@users.noreply.github.com>
2024-03-18 11:13:10 +0900

    [Bronze V] Title: A+B - 5, Time: 136 ms, Memory: 9572 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -0,0 +1,12 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+let hap = 0;
+
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < input[i].length; j++) {
+    if (parseInt(input[i][j]) || parseInt(input[i][j] === 0))
+      hap += parseInt(input[i][j]);
+  }
+  console.log(hap === 0 ? "" : hap);
+  hap = 0;
+}
