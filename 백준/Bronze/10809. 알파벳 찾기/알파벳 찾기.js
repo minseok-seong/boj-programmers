53ee730e094cf3dbb64300328875f3459b9b83c3
민석 <70144179+minseok-seong@users.noreply.github.com>
2024-03-20 10:53:04 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 132 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim();
+//97빼면서 찰코드앳에서
+
+const obj = {
+  1: "a",
+  2: "b",
+  3: "c",
+  4: "d",
+  5: "e",
+  6: "f",
+  7: "g",
+  8: "h",
+  9: "i",
+  10: "j",
+  11: "k",
+  12: "l",
+  13: "m",
+  14: "n",
+  15: "o",
+  16: "p",
+  17: "q",
+  18: "r",
+  19: "s",
+  20: "t",
+  21: "u",
+  22: "v",
+  23: "w",
+  24: "x",
+  25: "y",
+  26: "z",
+};
+
+const result = {};
+for (let k in obj) {
+  result[k - 1] = input.indexOf(obj[k]);
+}
+
+console.log(Object.values(result).join(" "));
