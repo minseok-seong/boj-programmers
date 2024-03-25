11f819d63c5e38aa50b8bb71342310dba370c755
민석 <70144179+minseoook@users.noreply.github.com>
2024-03-25 16:19:47 +0900

    [Bronze I] Title: 약수, Time: 188 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+input.shift();
+
+let arr = input
+  .join("")
+  .split(" ")
+  .map((a) => Number(a))
+  .sort((a, b) => a - b);
+
+let result = 0;
+if (arr.length % 2 === 0) {
+  result =
+    arr[Math.floor(arr.length / 2) - 1] * arr[Math.floor(arr.length / 2)];
+} else if (arr.length === 1) {
+  result = arr[Math.floor(arr.length / 2)] * arr[Math.floor(arr.length / 2)];
+} else if (arr.length % 2 === 1) {
+  result = arr[Math.floor(arr.length / 2)] * arr[Math.floor(arr.length / 2)];
+}
+console.log(result);
