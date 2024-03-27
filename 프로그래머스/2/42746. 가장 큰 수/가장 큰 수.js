604b395e370607911fe8835c11b6db44f3e1a5aa
민석 <70144179+minseoook@users.noreply.github.com>
2024-03-27 17:15:44 +0900

    [level 2] Title: 가장 큰 수, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,21 @@
+function solution(numbers) {
+ 
+    const arr =  numbers.map(a =>a+"").sort((a,b) => {
+        if(a[0]> b[0]){
+            return -1;
+        }else if(a[0] ===b[0]){
+            const hap1 = +(a+b)
+            const hap2 = +(b+a)
+            if(hap1 >hap2){
+                return -1
+            }else{
+                return 1
+            }
+        }else{
+            return 1
+        }
+    })
+    const hap = arr.reduce((a,b) =>a+(+b),0)
+    return hap ===0 ? "0" : arr.join("")
+   
+}
