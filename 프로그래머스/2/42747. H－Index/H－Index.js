e08f7d4dcbd4ab56933f823ce3bbc750f4c009cd
민석 <70144179+minseoook@users.noreply.github.com>
2024-04-01 14:21:59 +0900

    [level 2] Title: H-Index, Time: 3.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,22 +1,22 @@
 function solution(citations) {
-    var answer = 0;
-   const obj = {}
-   const max = Math.max(...citations)
-   for(let i=0;i<=max;i++){
-       let count = 0
-       for(let j=0;j<citations.length;j++){
-           if(i <citations[j]){
-               count++
-           }
-       }
-       obj[i] = count
-   }
-    console.log(obj)
-  for(let k in obj){
-      if(obj[k] ===(+k)){
-          answer = k
+  var answer = 0;
+  const obj = {};
+  const max = Math.max(...citations);
+  for (let i = 0; i <= max; i++) {
+    let count = 0;
+    for (let j = 0; j < citations.length; j++) {
+      if (i < citations[j]) {
+        count++;
       }
+    }
+    obj[i] = count;
   }
-    
-    return +answer;
+  console.log(obj);
+  for (let k in obj) {
+    if (obj[k] === +k) {
+      answer = k;
+    }
+  }
+
+  return +answer;
 }
