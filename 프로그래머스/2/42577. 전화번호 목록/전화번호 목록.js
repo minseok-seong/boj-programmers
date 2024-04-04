a8c05aa433762f29275c7b21fedac134a2de9704
민석 <70144179+minseoook@users.noreply.github.com>
2024-04-04 16:02:16 +0900

    [level 2] Title: 전화번호 목록, Time: 123.40 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,22 +1,15 @@
 function solution(phone_book) {
-    const map = new Map();
+    let answer = true;
+    let arr = [...phone_book];
 
-    // Map에 전화번호를 키로 추가
-    for (let i = 0; i < phone_book.length; i++) {
-        const phoneNumber = phone_book[i];
-        map.set(phoneNumber, true);
-    }
+    arr = arr.sort();
 
-    // 각 전화번호에 대해 다른 전화번호의 접두어인지 확인
-    for (let i = 0; i < phone_book.length; i++) {
-        const phoneNumber = phone_book[i];
-        for (let j = 1; j < phoneNumber.length; j++) {
-            const prefix = phoneNumber.slice(0, j);
-            if (map.has(prefix)) {
-                return false;
-            }
+    for(let i = 0; i < arr.length - 1; i++) {
+        let nextString = arr[i+1].substr(0, arr[i].length);
+        if (arr[i] === nextString){
+            return false;
         }
     }
 
-    return true;
+    return answer;
 }
