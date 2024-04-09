8860501093ca9cfe5c477a3ab5508f20fa05523b
민석 <70144179+minseoook@users.noreply.github.com>
2024-04-09 15:40:24 +0900

    [level 2] Title: 전화번호 목록, Time: 482.13 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,11 +1,27 @@
-function solution(phone_book) {
-  phone_book.sort();
-  for (let i = 0; i < phone_book.length - 1; i++) {
-    let a = phone_book[i + 1].slice(0, phone_book[i].length);
-    if (phone_book[i] === a) {
-      return false;
-    }
-  }
+// function solution(phone_book) {
+//   phone_book.sort();
+//   for (let i = 0; i < phone_book.length - 1; i++) {
+//     let a = phone_book[i + 1].slice(0, phone_book[i].length);
+//     if (phone_book[i] === a) {
+//       return false;
+//     }
+//   }
 
-  return true;
+//   return true;
+// }
+function solution(phone_book) {
+    const phoneDict = {}
+    
+    phone_book.forEach(phoneNumber => {
+        phoneDict[phoneNumber] = true
+    })
+
+    for(const phoneNumber of phone_book) {
+        for(let i = 1 ; i < phoneNumber.length ; i ++) {
+            const curStr = phoneNumber.slice(0, i)
+            if(phoneDict[curStr]) return false
+        }
+    }
+    
+    return true
 }
