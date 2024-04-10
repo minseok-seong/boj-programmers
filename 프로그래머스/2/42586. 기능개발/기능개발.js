6a06da55dea53ea6f15f427eb0c4cf5498b6edb8
민석 <70144179+minseoook@users.noreply.github.com>
2024-04-10 19:01:41 +0900

    [level 2] Title: 기능개발, Time: 3.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,35 @@
+function solution(progresses, speeds) {
+    var answer = [];
+    const result = []
+    for(let i=0;i<speeds.length;i++){
+        let n = 0
+        if((100-progresses[i]) % speeds[i] >0){
+            n=1
+        }
+        answer.push(Math.floor((100-progresses[i]) / speeds[i] +n) )
+    }
+    console.log(answer)
+     let count =1
+      let flag = answer[0]
+    for(let i=1;i<answer.length;i++){
+       
+       
+        if(flag >=answer[i]){
+            count++
+         
+          
+        }else if(flag < answer[i]){
+           
+            result.push(count)
+            count=1
+            flag = answer[i]
+           
+            
+        }
+        if(i === answer.length-1){
+           
+            result.push(count)
+        }
+    }
+    return result;
+}
