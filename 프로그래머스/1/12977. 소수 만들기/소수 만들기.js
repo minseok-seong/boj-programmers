604737defb9f57b65777c633b2a65a9f1df7f55f
민석 <70144179+minseoook@users.noreply.github.com>
2024-04-26 19:10:47 +0900

    [level 1] Title: 소수 만들기, Time: 0.06 ms, Memory: 32.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,31 @@
+function solution(nums) {
+    var answer = 0;
+    
+   for(let i=0;i<nums.length;i++){
+       for(let j=i+1;j<nums.length;j++){
+           for(let k=j+1;k<nums.length;k++){
+               if(isPrime(nums[i]+nums[j]+nums[k])){
+                   answer++
+               }
+           }
+       }
+   }
+  
+    
+    return answer;
+}
+
+function isPrime(num){
+    let count = 0;
+    for(let i=1;i<=num;i++){
+        if(num % i ==0){
+            count++
+        }
+    }
+    if(count ===2){
+        return true
+    }else{
+        return false
+    }
+    
+}
