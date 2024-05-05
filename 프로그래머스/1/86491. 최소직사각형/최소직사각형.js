58faa475cffdd3f9f7a20352649df363a7327690
민석 <70144179+minseoook@users.noreply.github.com>
2024-05-05 18:37:58 +0900

    [level 1] Title: 최소직사각형, Time: 3.90 ms, Memory: 39.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,21 +1,5 @@
+
 function solution(sizes) {
-    var answer = 0;
-    var arr =[];
-    
-    for(let i=0;i<sizes.length;i++){       
-        arr.push(sizes[i].sort((a,b) => b-a));
-    }
-    console.log(arr)
-    var width = arr[0][0];
-    var height = arr[0][1];
-    for(let i=1;i<arr.length;i++){
-        if(width < arr[i][0]){
-            width = arr[i][0]
-        }
-       if(height < arr[i][1]){
-            height = arr[i][1]
-        }
-    }
-    console.log(width,height)
-    return width*height;
+    const [hor, ver] = sizes.reduce(([h, v], [a, b]) => [Math.max(h, Math.max(a, b)), Math.max(v, Math.min(a, b))], [0, 0])
+    return hor * ver;
 }
